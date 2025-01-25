4ed7409bec700cd797d4073ad212a911ca1bc65f
Misyk8925 <129992573+Misyk8925@users.noreply.github.com>
2025-01-25 19:40:21 +0100

    first commit with gateway, discovery and servers configs

---
+++ discovery/src/test/java/com/misha/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misha.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
